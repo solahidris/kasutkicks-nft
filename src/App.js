3042df7db179a332b980082e775eb66ca10481b4
solahidris <emailsolah@gmail.com>
2023-09-22 20:12:43 +0800

    added keys

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ThirdwebSDKProvider } from '@thirdweb-dev/react';
+import { ChainId } from '@thirdweb-dev/sdk';
+import { ethers } from 'ethers';
 
-function App() {
+// change the variables
+
+const RELAYER_URL = 'YOUR_RELAYER_URL_HERE'; // Replace with your relayer URL
+
+const MintButton = () => {
+  const handleMint = async () => {
+    // Here, you'd call the minting function of your NFT contract.
+    // This is a placeholder and will need to be replaced with your actual minting logic.
+    console.log('Minting NFT...');
+  };
+
+  return <button onClick={handleMint}>Mint NFT</button>;
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Mint Your NFT</h1>
+      <MintButton />
     </div>
   );
-}
+};
 
-export default App;
+const ThirdwebProviderWrapper = () => {
+  const signer = new ethers.providers.Web3Provider(window.ethereum).getSigner();
+
+  return (
+    <ThirdwebSDKProvider
+      desiredChainId={ChainId.Mainnet} // or ChainId.Mumbai or any other supported chain
+      signer={signer}
+      sdkOptions={{
+        gasless: {
+          openzeppelin: {
+            relayerUrl: RELAYER_URL,
+          },
+        },
+      }}
+    >
+      <App />
+    </ThirdwebSDKProvider>
+  );
+};
+
+export default ThirdwebProviderWrapper;
